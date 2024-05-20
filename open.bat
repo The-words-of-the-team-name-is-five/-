0a653d362793a4bac6c2fbeb1b334a4b09e52304
weilong181 <161993848+weilong181@users.noreply.github.com>
2024-05-20 19:53:20 +0800

    Delete open.bat

--- open.bat
+++
@@ -1,8 +0,0 @@
-
-start cmd /k"cd C:\Users\Administrator\Desktop\online-mall&&npm install&&npm run dev"
-
-timeout /t 16 /nobreak >NUL
-
-for /f "tokens=3,4" %%a in ('"reg query HKEY_CLASSES_ROOT\http\shell\open\command"') do (set SoftWareRoot=%%a %%b)
-
-start "" % SoftWareRoot % "http://localhost:8888/"
